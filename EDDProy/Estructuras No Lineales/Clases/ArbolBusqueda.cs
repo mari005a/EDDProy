c4302c87fbf8376e5c30e1e12f87b3355e358c10
HP <HP@DESKTOP-JEJKNEO>
2024-10-21 22:08:30 -0700

    Agregar archivos de proyecto.

---
+++ EDDProy/Estructuras No Lineales/Clases/ArbolBusqueda.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDDemo.Estructuras_No_Lineales
+{
+    public class ArbolBusqueda
+    {
+        NodoBinario Raiz;
+        public String strArbol;
+
+        public ArbolBusqueda()
+        {
+            Raiz = null;
+            strArbol = "";
+        }
+
+        public Boolean EstaVacio()
+        {
+            if (Raiz == null)
+                return true;
+            else
+                return false;
+        }
+        public NodoBinario RegresaRaiz()
+        {
+            return Raiz;
+        }
+
+        public void InsertaNodo(int Dato, ref NodoBinario Nodo)
+        {            
+            if (Nodo == null)
+            {
+                Nodo = new NodoBinario(Dato);
+                // CAMBIO 2
+
+                if (Raiz == null)
+                    Raiz = Nodo;
+            }
+            else if (Dato < Nodo.Dato)
+                InsertaNodo(Dato, ref Nodo.Izq);
+            else if (Dato > Nodo.Dato)
+                InsertaNodo(Dato, ref Nodo.Der);          
+        }
+        public void Muestra(int nivel, NodoBinario nodo )
+        {
+            if (nodo == null)
+                return;
+            Muestra(nivel + 1, nodo.Der);
+            for(int i=0; i<nivel; i++)
+            {
+                strArbol = strArbol + "      ";
+            }
+            strArbol = strArbol + nodo.Dato.ToString() + "\r\n";
+            Muestra(nivel + 1, nodo.Izq); 
+        }
+    }
+}
